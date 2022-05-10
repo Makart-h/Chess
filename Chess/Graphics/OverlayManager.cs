a56033e87141dfdabec0784187f5ae75066e1a4a
Makart-h <90927967+Makart-h@users.noreply.github.com>
2022-05-10 23:27:21 +0200

    Update OverlayManager.cs

--- Chess/Graphics/OverlayManager.cs
+++ Chess/Graphics/OverlayManager.cs
@@ -4,6 +4,16 @@ namespace Chess.Graphics
 {
   public class OverlayManager
   {
-   
+   private readonly List<SquareOverlay> selections;
+   private readonly List<SquareOverlay> moves;
+
+   public void OnPieceSelected(object sender, PieceSelectedEventArgs args)
+   {
+
+   }
+   public void OnPieceMoved(object sender, PieceMovedEventArgs args)
+   {
+   }
+
   }
 }
