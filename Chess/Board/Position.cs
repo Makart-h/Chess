a35ea7a0fcf12e215a09bf1ad23467a110801a76
Makart-h <90927967+Makart-h@users.noreply.github.com>
2022-05-18 20:50:47 +0200

    Update Position.cs

--- Chess/Board/Position.cs
+++ Chess/Board/Position.cs
@@ -9,7 +9,7 @@ namespace Chess.Board
     private Team activeTeam;
     private int halfMoves;
     
-    public Position(Position other)
+    public Position(Position other, Move move)
     {
      foreach(var square in other.pieces.Keys)
      {
@@ -17,6 +17,18 @@ namespace Chess.Board
      }
      activeTeam = other.activeTeam == Team.White ? Team.Black : Team.White;
      halfMoves = other.halfMoves;
+     ApplyMove(move);
+    }
+    private void ApplyMove(Move move)
+    {
+      //move a LightPiece
+    }
+    private Update()
+    {
+     foreach(var item in pieces.Keys)
+     {
+      pieces[item]?.Update(); 
+     }
     }
   }
 }
