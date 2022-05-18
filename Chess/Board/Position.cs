96fd341994fc098436a794eb3c2b2113ba8f088b
Makart-h <90927967+Makart-h@users.noreply.github.com>
2022-05-18 20:18:13 +0200

    Update Position.cs

--- Chess/Board/Position.cs
+++ Chess/Board/Position.cs
@@ -8,5 +8,15 @@ namespace Chess.Board
     private Dictionary<Square, LightPiece> pieces;
     private Team activeTeam;
     private int halfMoves;
+    
+    public Position(Position other)
+    {
+     foreach(var square in other.pieces.Keys)
+     {
+      //create new LightPiece in LightPieceFactory
+     }
+     activeTeam = other.activeTeam == Team.White ? Team.Black : Team.White;
+     halfMoves = other.halfMoves;
+    }
   }
 }
