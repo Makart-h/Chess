7a05ff8b513684ec1b30ca0cf06a3aab47318477
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-09-11 21:13:33 +0200

    Add tools to convey the information on the result of the game

---
+++ Chess/AI/GameResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chess.AI
+{
+    public enum GameResult : byte
+    {
+        White,
+        Black,
+        Stalemate,
+        Draw,
+        InProgress,
+        ThreefoldRepetition,
+        FivefoldRepetition
+    }
+}
---
+++ Chess/AI/GameResultEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.AI
+{
+    internal class GameResultEventArgs
+    {
+        public GameResult GameResult;
+
+        public GameResultEventArgs(GameResult gameResult)
+        {
+            GameResult = gameResult;
+        }
+    }
+}
