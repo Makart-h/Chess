37c9413d43c2f14a986f8aef359e8290f94129d7
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-09-11 21:54:30 +0200

    Refactor class to better reflect it's purpose
    Change class to static since there is no reason for this class to be instantiated multiply times. Add initialization logic and prevent the usage of class before initialization.
    
    Add feature for clock to cancel AI calculations when it's timer elapsed

--- Chess/Clock/ChessClock.cs
+++ Chess/Clock/ChessClock.cs
@@ -1,65 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Timers;
 using Chess.AI;
 using Chess.Pieces;
 
 namespace Chess.Clock
 {
-    internal class ChessClock
+    internal static class ChessClock
     {
-        private readonly Dictionary<Team, Controller> controllers;
-        private readonly Dictionary<Team, ClockTimer> timers;
-        private readonly Dictionary<Team, Double> increment;
-        private Team activeTeam;
-        public Controller ActiveController { get => controllers[activeTeam]; }
+        private static readonly Dictionary<Team, Controller> _controllers;
+        private static readonly Dictionary<Team, ClockTimer> _timers;
+        private static readonly Dictionary<Team, Double> _increment;
+        private static readonly Dictionary<Controller, CancellationTokenSource> _cancelletionTokenSources;
+        private static bool _isRunning;
+        private static bool _isInitialized;
+        private static Team _activeTeam;
+        public static Controller ActiveController
+        {
+            get
+            {
+                CheckInitialization();
+                return _controllers[_activeTeam];              
+            }
+        }
         public static event EventHandler<TimerExpiredEventArgs> TimerExpired;
-
-        public ChessClock(Controller white, Controller black, TimeSpan interval, TimeSpan increment, Team activeTeam)
+        static ChessClock()
         {
-            controllers = new Dictionary<Team, Controller>();
-            timers = new Dictionary<Team, ClockTimer>();
-            this.increment = new Dictionary<Team, double>();
-            this.activeTeam = activeTeam;
-            this.increment[Team.White] = increment.TotalMilliseconds;
-            this.increment[Team.Black] = increment.TotalMilliseconds;
-            controllers[Team.White] = white;
-            controllers[Team.Black] = black;
-            timers[Team.White] = new ClockTimer(interval.TotalMilliseconds);
-            timers[Team.White].Elapsed += OnTimerExpired;
-            timers[Team.Black] = new ClockTimer(interval.TotalMilliseconds);
-            timers[Team.Black].Elapsed += OnTimerExpired;
-            Controller.MoveChosen += OnMoveChosen;
-            Controller.NoMovesAvailable += OnNoMovesAvailable;
+            _isRunning = false;
+            _isInitialized = false;
+            _controllers = new Dictionary<Team, Controller>();
+            _timers = new Dictionary<Team, ClockTimer>();
+            _cancelletionTokenSources = new Dictionary<Controller, CancellationTokenSource>();
+            _increment = new Dictionary<Team, double>();
         }
-
-        public void OnMoveChosen(object sender, MoveChosenEventArgs args) => Toggle();
-
-        public void OnTimerExpired(object sender, ElapsedEventArgs args)
+        public static void Initialize(Controller white, Controller black, TimeSpan interval, TimeSpan increment, Team activeTeam)
         {
-            activeTeam = ~activeTeam;
-            timers[activeTeam].Stop();
-            TimerExpired?.Invoke(this, new TimerExpiredEventArgs(controllers[activeTeam]));
+            _activeTeam = activeTeam;
+            AddCancelletionTokenSource(white, black);
+            SetIncrement(increment);
+            SetControllers(white: white, black: black);
+            CreateTimers(interval);
+            SubscribeToEvents();
+            _isInitialized = true;
         }
-        private void OnNoMovesAvailable(object sender, NoMovesEventArgs args)
+        private static void SetIncrement(TimeSpan increment)
         {
-            timers[args.Team].Pause();
-            activeTeam = ~args.Team;
-            timers[activeTeam].Pause();
+            _increment[Team.White] = increment.TotalMilliseconds;
+            _increment[Team.Black] = increment.TotalMilliseconds;
         }
-        public void Start()
+        private static void SetControllers(Controller white, Controller black)
         {
-            timers[activeTeam].Start();
+            _controllers[Team.White] = white;
+            _controllers[Team.Black] = black;
         }
-        private void Toggle()
+        private static void CreateTimers(TimeSpan interval)
         {
-            timers[activeTeam].Pause();
-            timers[activeTeam].Increment(increment[activeTeam]);
-            activeTeam = ~activeTeam;
-            timers[activeTeam].Resume();
+            _timers[Team.White] = new ClockTimer(interval.TotalMilliseconds);
+            _timers[Team.Black] = new ClockTimer(interval.TotalMilliseconds);
         }
-
-        public (TimeSpan WhiteTimer, TimeSpan BlackTimer) GetTimers() => (TimeSpan.FromMilliseconds(timers[Team.White].RemainingTime), TimeSpan.FromMilliseconds(timers[Team.Black].RemainingTime));
+        private static void SubscribeToEvents()
+        {
+            _timers[Team.White].Elapsed += OnTimerExpired;
+            _timers[Team.Black].Elapsed += OnTimerExpired;
+            Controller.MoveMade += OnMoveChosen;
+            Arbiter.GameConcluded += OnGameConcluded;
+        }
+        private static void CheckInitialization()
+        {
+            if (!_isInitialized)
+                throw new InvalidOperationException("ChessClock not initilized!");
+        }
+        private static void AddCancelletionTokenSource(params Controller[] controllers)
+        {
+            foreach (Controller controller in controllers)
+            {
+                if (controller is AIController)
+                    _cancelletionTokenSources[controller] = new CancellationTokenSource();
+            }
+        }
+        private static void OnMoveChosen(object sender, MoveMadeEventArgs e) => Toggle();
+        private static void OnTimerExpired(object sender, ElapsedEventArgs e)
+        {
+            if (_cancelletionTokenSources.TryGetValue(_controllers[_activeTeam], out CancellationTokenSource source))
+                source.Cancel();
+            StopAllTimers();                   
+            TimerExpired?.Invoke(null, new TimerExpiredEventArgs(_controllers[~_activeTeam]));
+        }
+        private static void OnGameConcluded(object sender, GameResultEventArgs e) => StopAllTimers();
+        private static void StopAllTimers()
+        {
+            _isRunning = false;
+            _timers[_activeTeam].Pause();
+            _timers[~_activeTeam].Pause();
+        }      
+        private static void Toggle()
+        {
+            if (_isRunning)
+            {
+                _timers[_activeTeam].Pause();
+                _timers[_activeTeam].Increment(_increment[_activeTeam]);
+                _activeTeam = ~_activeTeam;
+                _timers[_activeTeam].Resume();
+            }
+        }
+        public static (TimeSpan WhiteTimer, TimeSpan BlackTimer) GetTimers()
+        {
+            CheckInitialization();
+            return (TimeSpan.FromMilliseconds(_timers[Team.White].RemainingTime), TimeSpan.FromMilliseconds(_timers[Team.Black].RemainingTime));               
+        }
+        public static void Start()
+        {
+            CheckInitialization();
+            if (!_isRunning)
+            {
+                _isRunning = true;
+                _timers[_activeTeam].Start();
+            }
+        }
+        public static CancellationToken? GetCancelletionToken(Controller controller)
+        {
+            CheckInitialization();
+            if (_cancelletionTokenSources.TryGetValue(controller, out var source))
+                return source.Token;
+            else
+                return null;
+        }
     }
 }
