c24c84d3f219d06792439c9ce00ac58ce9af2fd3
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-09-20 20:39:59 +0200

    sort using statments

--- Chess/Movement/Initiator.cs
+++ Chess/Movement/Initiator.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using Chess.Board;
+﻿using Chess.Board;
 using Chess.Graphics;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace Chess.Movement
 {
