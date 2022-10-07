5e570747c34ab4569156e66081215a81d5288179
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-10-07 23:20:50 +0200

    add feature to handle the input of any keyboard key

---
+++ Chess/Input/InputManager.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Chess.Input;
+
+internal sealed class InputManager : IDisposable
+{
+
+    private readonly Dictionary<Keys, KeyInput> _inputEvents;
+    private readonly Stack<Keys> _keysToCheckForRealease;
+    private const double _defaultCooldownInMilliseconds = 500d;
+    private bool _isDisposed;
+    public InputManager()
+    {
+        _inputEvents = new();
+        _keysToCheckForRealease = new();
+    }
+    public void SubscribeToEvent(Keys key, Action method, double cooldownInMilliseconds = _defaultCooldownInMilliseconds)
+    {
+        ThrowIfDisposed();
+        if (_inputEvents.TryGetValue(key, out var value))
+            value.AddAction(method);
+        else
+            _inputEvents[key] = new KeyInput(method, cooldownInMilliseconds);
+    }
+    public void CheckInput(GameTime gameTime)
+    {
+        ThrowIfDisposed();
+        CheckReleasedKeys();
+        CheckPressedKeys(gameTime);
+    }
+    private void ThrowIfDisposed()
+    {
+        if (_isDisposed)
+            throw new ObjectDisposedException(nameof(InputManager));
+    }
+    private void CheckReleasedKeys()
+    {
+        foreach (Keys key in _keysToCheckForRealease)
+        {
+            if (Keyboard.GetState().IsKeyUp(key))
+                _inputEvents[key].Release();
+        }
+    }
+    private void CheckPressedKeys(GameTime gameTime)
+    {
+        foreach (Keys key in Keyboard.GetState().GetPressedKeys())
+        {
+            if (_inputEvents.TryGetValue(key, out var value))
+            {
+                if (value.TryPress(gameTime))
+                    _keysToCheckForRealease.Push(key);
+            }
+        }
+    }
+    public void Dispose()
+    {
+        if (_isDisposed)
+            return;
+
+        foreach(Keys key in _inputEvents.Keys)
+        {
+            _inputEvents[key].Dispose();
+        }
+        _inputEvents.Clear();
+        _isDisposed = true;
+    }
+}
---
+++ Chess/Input/KeyInput.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Chess.Input;
+
+internal sealed class KeyInput : IDisposable
+{
+    private bool _isPressed;
+    private double _timeSinceClick;
+    private readonly double _cooldownInMilliseconds;
+    private Action _onPressed;
+    private bool _isDisposed;
+
+    public KeyInput(Action onPressed, double cooldownInMilliseconds)
+    {
+        _onPressed = new Action(onPressed);
+        _cooldownInMilliseconds = cooldownInMilliseconds;
+    }
+    public void AddAction(Action action)
+    {
+        if (action != null)
+            _onPressed += action;
+    }
+    public bool TryPress(GameTime gameTime)
+    {
+        if (!_isPressed || _timeSinceClick >= _cooldownInMilliseconds)
+        {
+            _isPressed = true;
+            _timeSinceClick = 0;
+            _onPressed?.Invoke();
+            return true;
+        }
+        else
+        {
+            _timeSinceClick += gameTime.ElapsedGameTime.TotalMilliseconds;
+            return false;
+        }
+    }
+    public void Release()
+    {
+        _isPressed = false;
+        _timeSinceClick = 0;
+    }
+    public void Dispose()
+    {
+        if(_isDisposed)
+            return;
+
+        _isDisposed = true;
+        _onPressed = null;
+    }
+}
