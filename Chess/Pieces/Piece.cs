7312f7f40563f392d891da0489317c243909e60e
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-10-18 23:46:31 +0200

    refactor

--- Chess/Pieces/Piece.cs
+++ Chess/Pieces/Piece.cs
@@ -17,7 +17,8 @@ internal abstract class Piece : IComparable<Piece>
     public static event EventHandler<PieceEventArgs> PieceSelected;
     public static event EventHandler<PieceEventArgs> PieceDeselected;
     public int Value { get; protected set; }
-    public MoveSets MoveSet { get => _moveSet; }
+    public Movesets Moveset { get => _moveset; }
+    public PieceType Type { get; init; }
     public bool IsSelected
     {
         get => _isSelected;
@@ -26,18 +27,18 @@ internal abstract class Piece : IComparable<Piece>
             _isSelected = value;
             if (!IsRaw)
             {
-            if (_isSelected)
-                OnPieceSelected(new PieceEventArgs(this));
-            else
+                if (_isSelected)
+                    OnPieceSelected(new PieceEventArgs(this));
+                else
                     OnPieceDeselected(new PieceEventArgs(this));
+            }
         }
-    }
     }
     public bool IsRaw { get; protected set; }
     public Square Square { get; set; }
-    protected List<Move> moves;
+    protected List<Move> _moves;
 
-    protected Piece(Team team, Square square, PieceType type, bool isRaw)
+    protected Piece(Team team, Square square, PieceType type, bool isRaw)     
     {
         IsRaw = isRaw;
         _team = team;
@@ -55,19 +56,10 @@ internal abstract class Piece : IComparable<Piece>
         Value = other.Value;
         Owner = other.Owner;
     }
-    public List<Move> Moves { get => moves; set => moves = value; }
-    protected List<Move> CreateCopyOfMoves()
+    public List<Move> Moves { get => _moves; set => _moves = value; }
+    public Move? GetAMove(Square destination)
     {
-        List<Move> copy = new(Moves.Count);
-        foreach (var move in moves)
-        {
-            copy.Add(new Move(move));
-        }
-        return copy;
-    }
-    public Move GetAMove(Square destination)
-    {
-        foreach (Move move in moves)
+        foreach (Move move in _moves)
         {
             if (move.Latter == destination)
                 return move;
@@ -76,46 +68,29 @@ internal abstract class Piece : IComparable<Piece>
     }
     public virtual void CheckPossibleMoves()
     {
-        (MoveSets sets, Move[] moves)[] groupedMoves = MovementManager.GenerateEveryMove(Square, _moveSet, Owner);
-        foreach (var group in groupedMoves)
-        {
-            foreach (Move move in group.moves)
-            {
-                if (Owner.GetKing(_team).CheckMoveAgainstThreats(this, move))
-                    moves.Add(move);
-            }
-        }
+        _moves = MovementManager.GenerateEveryMove(Square, _moveset, Owner, friendlyFire: IsRaw);
+        _moves = Owner.GetKing(_team).FilterMovesThroughThreats(_moves);
     }
-    public virtual void MovePiece(Move move)
+    public virtual void MovePiece(in Move move)
     {
-        OnPieceMoved(new PieceMovedEventArgs(this, move));
         Square = move.Latter;
+        if(!IsRaw)
+            OnPieceMoved(new PieceMovedEventArgs(this, move));       
     }
-    public virtual int Update()
+    public virtual void Update()
     {
-        moves.Clear();
         CheckPossibleMoves();
-        return moves.Count;
     }
     protected virtual void OnPieceMoved(PieceMovedEventArgs e)
     {
-        if (IsRaw)
-            return;
-
         PieceMoved?.Invoke(this, e);
     }
     protected virtual void OnPieceSelected(PieceEventArgs e)
     {
-        if (IsRaw)
-            return;
-
         PieceSelected?.Invoke(this, e);
     }
-    protected virtual void OnPieceDeselected(EventArgs e)
+    protected virtual void OnPieceDeselected(PieceEventArgs e)
     {
-        if (IsRaw)
-            return;
-
         PieceDeselected?.Invoke(this, e);
     }
     public int CompareTo(Piece other)
