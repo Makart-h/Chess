27d67780cfbeac773feb1ce694ad2fe2704ed7b9
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-10-18 23:44:09 +0200

    optimize

--- Chess/Pieces/Pawn.cs
+++ Chess/Pieces/Pawn.cs
@@ -2,6 +2,7 @@
 using Chess.Movement;
 using Chess.Pieces.Info;
 using System;
+using System.Collections.Generic;
 
 namespace Chess.Pieces;
 
@@ -10,6 +11,10 @@ internal sealed class Pawn : Piece
     private bool _hasMoved;
     private bool _enPassant;
     private readonly int _promotionSquareNumber;
+    private Square _leftEnPassant;
+    private Square _rightEnPassant;
+    private Square _oneUp;
+    private readonly Square _doubleMove;
     public static EventHandler Promotion;
     public bool EnPassant { get { return _enPassant; } set { _enPassant = value; } }
     public Pawn(Team team, Square square, bool isRaw = false) : base(team, square, PieceType.Pawn, isRaw)
@@ -20,95 +25,79 @@ internal sealed class Pawn : Piece
         Value = team == Team.White ? 1 : -1;
         if (_promotionSquareNumber == 8 && square.Digit != 2 || _promotionSquareNumber == 1 && square.Digit != 7)
             _hasMoved = true;
+
+        _leftEnPassant = new Square(Square, (-1, 0));
+        _rightEnPassant = new Square(Square, (1, 0));
+        _oneUp = new Square(Square, (0, Value));
+        _doubleMove = new Square(Square, (0, Value * 2));
     }
     public Pawn(Pawn other, bool isRaw = false) : base(other, isRaw)
     {
         _enPassant = other._enPassant;
         _hasMoved = other._hasMoved;
         _promotionSquareNumber = other._promotionSquareNumber;
+        _leftEnPassant = other._leftEnPassant;
+        _rightEnPassant = other._rightEnPassant;
+        _oneUp = other._oneUp;
+        _doubleMove = other._doubleMove;
     }
-    public override int Update()
+    public override void Update()
     {
         if (_enPassant)
             _enPassant = false;
-        return base.Update();
+        base.Update();
     }
     public override void CheckPossibleMoves()
     {
-        Team teamOnTheSquare;
-        Square square;
-        int direction = _team == Team.White ? 1 : -1;
-
-        // Double move that's available at the start position.
-        CheckDoubleMove(direction);
-
+        _moves = new(2);
+        Team teamOneUp = Owner.GetTeamOnSquare(_oneUp);  
         // Regular move.
-        square = new Square(Square.Letter, Square.Digit + (1 * direction));
-        teamOnTheSquare = Owner.GetTeamOnSquare(square);
-        if (teamOnTheSquare == Team.Empty)
+        if (teamOneUp == Team.Empty)
         {
-            Move moveToAdd = new Move(Square, square, 'm');
-            if (Owner.GetKing(_team).CheckMoveAgainstThreats(this, moveToAdd))
-                moves.Add(moveToAdd);
+            // Double move that's available at the start position.
+            if (!_hasMoved)
+                CheckDoubleMove();
+            Move moveToAdd = new(Square, _oneUp, 'm');
+            _moves.Add(moveToAdd);
         }
-
-        // Captures.
-        base.CheckPossibleMoves();
-
         // En passant to the right.
-        CheckEnPassant(new Square((char)(Square.Letter + 1), Square.Digit), direction);
+        CheckEnPassant(_rightEnPassant);
         // En passant to the left.
-        CheckEnPassant(new Square((char)(Square.Letter - 1), Square.Digit), direction);
+        CheckEnPassant(_leftEnPassant);
+        // Add captures and filter through king.
+        _moves.AddRange(MovementManager.GenerateEveryMove(Square, _moveset, Owner, friendlyFire: IsRaw));
+        _moves = Owner.GetKing(_team).FilterMovesThroughThreats(_moves);
     }
-    private void CheckDoubleMove(int direction)
+    private void CheckDoubleMove()
     {
-        if (!_hasMoved)
-        {
-            Square square = new Square(Square.Letter, Square.Digit + 1 * direction);
-            if (Square.Validate(Square))
-            {
-                Team teamOnTheSquare = Owner.GetTeamOnSquare(square);
-                if (teamOnTheSquare == Team.Empty)
-                {
-                    square = new Square(Square.Letter, Square.Digit + 2 * direction);
-                    if (Square.Validate(square))
-                    {
-                        teamOnTheSquare = Owner.GetTeamOnSquare(square);
-                        if (teamOnTheSquare == Team.Empty)
-                        {
-                            Move moveToAdd = new Move(Square, square, 'm');
-                            if (Owner.GetKing(_team).CheckMoveAgainstThreats(this, moveToAdd))
-                                moves.Add(moveToAdd);
-                        }
-                    }
-                }
-            }
-        }
+        Team teamOnTheSecondSquare = Owner.GetTeamOnSquare(_doubleMove);
+        if (teamOnTheSecondSquare == Team.Empty)
+            _moves.Add(new Move(Square, _doubleMove, 'm'));
     }
-    private void CheckEnPassant(Square square, int direction)
+    private void CheckEnPassant(Square square)
     {
         if (Square.Validate(square))
         {
             Team teamOnTheSquare = Owner.GetTeamOnSquare(square);
             if (teamOnTheSquare != _team && teamOnTheSquare != Team.Empty)
             {
-                if (Owner.TryGetPiece(square, out Piece piece) && piece is Pawn p && p._enPassant == true)
-                {
-                    Move moveToAdd = new Move(Square, new Square(square.Letter, Square.Digit + (1 * direction)), 'p');
-                    if (Owner.GetKing(_team).CheckMoveAgainstThreats(this, moveToAdd))
-                        moves.Add(moveToAdd);
-                }
+                Piece piece = Owner.GetPiece(square);
+                if (piece is Pawn p && p._enPassant == true)
+                    _moves.Add(new(Square, new Square(square, (0, Value)), 'p'));
             }
         }
     }
-    public override void MovePiece(Move move)
+    public override void MovePiece(in Move move)
     {
-        if (Math.Abs(Square.Digit - move.Latter.Digit) == 2)
+        if (move.Latter == _doubleMove)
             _enPassant = true;
         Square = move.Latter;
         _hasMoved = true;
+        UpdateSquares(move.Former, move.Latter);
 
-        OnPieceMoved(new PieceMovedEventArgs(this, move));
+        if(!IsRaw)
+            OnPieceMoved(new PieceMovedEventArgs(this, move));
+
         if (Square.Digit == _promotionSquareNumber)
         {
             if (IsRaw)
@@ -116,5 +105,12 @@ internal sealed class Pawn : Piece
             else
                 Promotion?.Invoke(this, EventArgs.Empty);
         }       
+    }
+    private void UpdateSquares(Square oldSquare, Square newSquare)
+    {
+        var vector = newSquare - oldSquare;
+        _oneUp.Transform(vector);
+        _leftEnPassant.Transform(vector);
+        _rightEnPassant.Transform(vector);
     }
 }
