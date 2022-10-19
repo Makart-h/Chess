d9908e31e9b943ccda11633fbb8314c5f14bcae9
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-10-20 00:00:08 +0200

    add flags enum to remove code clutter associated with optional parameters

---
+++ Chess/Movement/MoveGeneratorOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chess.Movement;
+
+[Flags]
+internal enum MoveGeneratorOptions
+{
+    None = 0,
+    InfiniteRange = 1,
+    MovesIncluded = 1 << 1,
+    TakesIncluded = 1 << 2,
+    DefendsIncluded = 1 << 3
+}
