10b63da5f7126271765122777cd5d1ab0ff1127d
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-10-20 00:01:20 +0200

    drop move generating methods from MovementManager class and put them into newly created MoveGenerator class to respect SRP

---
+++ Chess/Movement/MoveGenerator.cs
@@ -0,0 +1,127 @@
+﻿using Chess.Board;
+using Chess.Pieces.Info;
+using Chess.Pieces;
+using System;
+using System.Collections.Generic;
+
+namespace Chess.Movement;
+
+internal class MoveGenerator
+{
+    private static readonly (int, int)[] _knightIterators;
+    private static readonly (int, int)[] _rookIterators;
+    private static readonly (int, int)[] _bishopIterators;
+    private static readonly (int, int)[] _everyDirectionIterators;
+    private static readonly MoveGeneratorOptions _defaultOptions;
+    static MoveGenerator()
+    {
+        _knightIterators = new[] { (2, 1), (2, -1), (-1, -2), (1, -2), (-2, -1), (-2, 1), (1, 2), (-1, 2) };
+        _rookIterators = new[] { (0, 1), (0, -1), (1, 0), (-1, 0) };
+        _bishopIterators = new[] { (1, 1), (1, -1), (-1, 1), (-1, -1) };
+        _everyDirectionIterators = new[] { (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1) };
+        _defaultOptions = MoveGeneratorOptions.TakesIncluded | MoveGeneratorOptions.MovesIncluded;
+    }
+    public static List<Move> GenerateEveryMove(Square initialSquare, Movesets set, IPieceOwner pieceOwner, bool friendlyFire = false)
+    {
+        List<Move> moves;
+        MoveGeneratorOptions options = _defaultOptions;
+        if (friendlyFire)
+            options |= MoveGeneratorOptions.DefendsIncluded;
+        // For a piece with a moveset of a pawn only the moves that lead to captures are considered.
+        if (set == Movesets.Pawn)
+        {
+            moves = new(2);
+            int direction = pieceOwner.GetTeamOnSquare(initialSquare) == Team.White ? 1 : -1;
+            (int, int)[] iterators = { (1, 1 * direction), (-1, 1 * direction) };
+            foreach (var iterator in iterators)
+                GenerateMovesInDirection(moves, initialSquare, iterator, pieceOwner, options ^ MoveGeneratorOptions.MovesIncluded);
+        }
+        else if (set == Movesets.Knight)
+        {
+            moves = new(4);
+            foreach (var iterator in _knightIterators)
+                GenerateMovesInDirection(moves, initialSquare, iterator, pieceOwner, options);
+        }
+        else if (set == Movesets.Bishop)
+        {
+            moves = new(7);
+            foreach (var iterator in _bishopIterators)
+                GenerateMovesInDirection(moves, initialSquare, iterator, pieceOwner, options | MoveGeneratorOptions.InfiniteRange);
+        }
+        else if (set == Movesets.Rook)
+        {
+            moves = new(7);
+            foreach (var iterator in _rookIterators)
+                GenerateMovesInDirection(moves, initialSquare, iterator, pieceOwner, options | MoveGeneratorOptions.InfiniteRange);
+        }
+        else if (set == Movesets.King)
+        {
+            moves = new(4);
+            foreach (var iterator in _everyDirectionIterators)
+                GenerateMovesInDirection(moves, initialSquare, iterator, pieceOwner, options);
+        }
+        else
+        {
+            moves = new(14);
+            foreach (var iterator in _everyDirectionIterators)
+                GenerateMovesInDirection(moves, initialSquare, iterator, pieceOwner, options | MoveGeneratorOptions.InfiniteRange);
+        }
+        return moves;
+    }
+    public static void GenerateMovesInDirection(List<Move> moves, Square initialSquare, (int letter, int digit) squareIterator, IPieceOwner pieceOwner, MoveGeneratorOptions options)
+    {
+        if ((options & MoveGeneratorOptions.InfiniteRange) == 0)
+        {
+            Square nextSquare = new(initialSquare, squareIterator);
+            if (nextSquare.IsValid)
+                GenerateMoveAndStop(moves, initialSquare, nextSquare, pieceOwner, options);
+        }
+        else
+        {
+            int numberOfIterations = GetNumberOfPossibleValidSquareIterations(initialSquare, squareIterator);
+            Square current = initialSquare;
+            for (int i = 0; i < numberOfIterations; ++i)
+            {
+                current.Transform(squareIterator);
+                if (GenerateMoveAndStop(moves, initialSquare, current, pieceOwner, options))
+                    break;
+            }
+        }
+    }
+    public static bool GenerateMoveAndStop(List<Move> moves, Square initialSquare, Square current, IPieceOwner pieceOwner, MoveGeneratorOptions options)
+    {
+        Piece pieceOnTheWay = pieceOwner.GetPiece(current);
+        if (pieceOnTheWay != null)
+        {
+            if ((options & MoveGeneratorOptions.TakesIncluded) != 0 && pieceOnTheWay.Team != pieceOwner.GetTeamOnSquare(initialSquare))
+                moves.Add(new Move(initialSquare, current, MoveType.Takes));
+            else if ((options & MoveGeneratorOptions.DefendsIncluded) != 0)
+                moves.Add(new Move(initialSquare, current, MoveType.Defends));
+            return true;
+        }
+        else if ((options & MoveGeneratorOptions.MovesIncluded) != 0)
+        {
+            moves.Add(new Move(initialSquare, current, MoveType.Moves));
+        }
+        return false;
+    }
+    public static int GetNumberOfPossibleValidSquareIterations(Square initialSquare, (int letter, int digit) squareIterator)
+    {
+        int letterValidIterations = int.MaxValue;
+        int digitValidIterations = int.MaxValue;
+
+        if (squareIterator.letter < 0)
+            letterValidIterations = (initialSquare.Letter - Square.MinLetter) / -squareIterator.letter;
+        else if (squareIterator.letter > 0)
+            letterValidIterations = (Square.MaxLetter - initialSquare.Letter) / squareIterator.letter;
+        if (squareIterator.digit < 0)
+            digitValidIterations = (initialSquare.Digit - Square.MinDigit) / -squareIterator.digit;
+        else if (squareIterator.digit > 0)
+            digitValidIterations = (Square.MaxDigit - initialSquare.Digit) / squareIterator.digit;
+
+        if (letterValidIterations == int.MaxValue && digitValidIterations == int.MaxValue)
+            return 0;
+        else
+            return Math.Min(letterValidIterations, digitValidIterations);
+    }
+}
--- Chess/Movement/MovementManager.cs
+++ Chess/Movement/MovementManager.cs
@@ -19,6 +19,7 @@ internal sealed class MovementManager : IDisposable
     private bool _rapidMovement;
     private bool _isDisposed;
     public float MovementVelocity { get; private set; }
+
     private MovementManager()
     {
         _initiators = new();
@@ -102,94 +103,4 @@ internal sealed class MovementManager : IDisposable
         return new Vector2(width - vector.X, height - vector.Y);
     }
     private static void OnMovementConcluded(EventArgs e) => MovementConcluded?.Invoke(null, e);
-    public static List<Move> GenerateEveryMove(Square initialSquare, Movesets set, IPieceOwner pieceOwner, bool friendlyFire = false)
-    {
-        List<Move> moves;
-        // For a piece with a moveset of a pawn only the moves that lead to captures are considered.
-        if (set == Movesets.Pawn)
-        {
-            moves = new(2);
-            int direction = pieceOwner.GetTeamOnSquare(initialSquare) == Team.White ? 1 : -1;
-            GenerateMovesInDirection(moves, initialSquare, (1, 1 * direction), pieceOwner, infiniteRange: false, movesIncluded: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 1 * direction), pieceOwner, infiniteRange: false, movesIncluded: false, friendlyFire: friendlyFire);
-        }
-        else if (set == Movesets.Knight)
-        {
-            moves = new(4);
-            GenerateMovesInDirection(moves, initialSquare, (2, 1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (2, -1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, -2), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, -2), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-2, -1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-2, 1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, 2), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 2), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-        }  
-        else if (set == Movesets.Bishop)
-        {
-            moves = new(7);
-            GenerateMovesInDirection(moves, initialSquare, (1, 1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, -1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, -1), pieceOwner, friendlyFire: friendlyFire);
-        }
-        else if (set == Movesets.Rook)
-        {
-            moves = new(7);
-            GenerateMovesInDirection(moves, initialSquare, (0, 1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (0, -1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, 0), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 0), pieceOwner, friendlyFire: friendlyFire);
-        }
-        else if (set == Movesets.King)
-        {
-            moves = new(4);
-            GenerateMovesInDirection(moves, initialSquare, (0, 1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (0, -1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, 0), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 0), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, 1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, -1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, -1), pieceOwner, infiniteRange: false, friendlyFire: friendlyFire);
-        }
-        else
-        {
-            moves = new(14);
-            GenerateMovesInDirection(moves, initialSquare, (1, 1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, -1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, -1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (0, 1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (0, -1), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (1, 0), pieceOwner, friendlyFire: friendlyFire);
-            GenerateMovesInDirection(moves, initialSquare, (-1, 0), pieceOwner, friendlyFire: friendlyFire);
-        }
-        return moves;
-    }
-    public static void GenerateMovesInDirection(List<Move> moves, Square initialSquare, (int letter, int digit) cordsIterator, IPieceOwner pieceOwner, bool infiniteRange = true, bool movesIncluded = true, bool friendlyFire = false)
-    {
-        Square current = initialSquare;
-        Piece pieceOnTheWay;
-        do
-        {
-            current.Transform(cordsIterator);
-            if (!Square.Validate(current))
-                break;
-
-            pieceOnTheWay = pieceOwner.GetPiece(current);
-            if (pieceOnTheWay != null)
-            {
-                if (pieceOnTheWay.Team != pieceOwner.GetTeamOnSquare(initialSquare))
-                    moves.Add(new Move(initialSquare, current, MoveType.Takes));
-                else if (friendlyFire)
-                    moves.Add(new Move(initialSquare, current, MoveType.Defends));
-                break;
-            }
-            else if (movesIncluded)
-            {
-                moves.Add(new Move(initialSquare, current, MoveType.Moves));
-            }
-        } while (infiniteRange);
-    }
 }
